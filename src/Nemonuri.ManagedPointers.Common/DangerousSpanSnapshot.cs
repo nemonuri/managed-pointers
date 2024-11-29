de2b900318216bbeb84ee7939d7968e4d5318eea
nemonuri <nemonuri2@gmail.com>
2024-11-29 18:44:56 +0900

    rename to DangerousSpanSnapshot

--- src/Nemonuri.ManagedPointers.Common/DangerousSpanSnapshot.cs
+++ src/Nemonuri.ManagedPointers.Common/DangerousSpanSnapshot.cs
@@ -1,18 +1,18 @@
 namespace Nemonuri.ManagedPointers;
 
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
-public readonly record struct SpanSnapshot<T>
+public readonly record struct DangerousSpanSnapshot<T>
 {
     private readonly nint pointer;
     private readonly int length;
 
-    internal SpanSnapshot(nint pointer, int length)
+    internal DangerousSpanSnapshot(nint pointer, int length)
     {
         this.pointer = pointer;
         this.length = length;
     }
 
-    public unsafe SpanSnapshot(in ReadOnlySpan<T> readOnlySpan) : this
+    public unsafe DangerousSpanSnapshot(in ReadOnlySpan<T> readOnlySpan) : this
     (
         pointer: (nint)Unsafe.AsPointer(ref MemoryMarshal.GetReference(readOnlySpan)),
         length: readOnlySpan.Length
