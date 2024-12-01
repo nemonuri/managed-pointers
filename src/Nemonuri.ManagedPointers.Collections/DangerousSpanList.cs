43210386160351d0f9972c7a2cc1d494f0dbfa37
nemonuri <nemonuri2@gmail.com>
2024-12-02 00:18:28 +0900

    .

---
+++ src/Nemonuri.ManagedPointers.Collections/DangerousSpanList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using CommunityToolkit.Diagnostics;
+
+namespace Nemonuri.ManagedPointers.Collections;
+
+public ref struct DangerousSpanList<T>
+{
+    private readonly ReadOnlySpan<DangerousSpanSnapshot<T>> _innerDangerousSpanSnapshotSpan;
+
+    public DangerousSpanList(in ReadOnlySpan<DangerousSpanSnapshot<T>> innerDangerousSpanSnapshotSpan)
+    {
+        _innerDangerousSpanSnapshotSpan = innerDangerousSpanSnapshotSpan;
+    }
+
+    public readonly ReadOnlySpan<DangerousSpanSnapshot<T>> InnerDangerousSpanSnapshotSpan => _innerDangerousSpanSnapshotSpan;
+
+    public readonly int Count => _innerDangerousSpanSnapshotSpan.Length;
+
+    public Span<T> this[int index]
+    {
+        get
+        {
+            Guard.IsInRange(index, 0, Count);
+            return _innerDangerousSpanSnapshotSpan[index].GetSpan();
+        }
+    }
+}
