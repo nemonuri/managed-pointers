1c6da2cebc11cc28bee7b01d51bee421a8d4dd48
nemonuri <nemonuri2@gmail.com>
2025-03-06 23:24:26 +0900

    .

--- src/Nemonuri.ManagedPointers.Common/ManagedPointerTheory.cs
+++ src/Nemonuri.ManagedPointers.Common/ManagedPointerTheory.cs
@@ -2,25 +2,37 @@
 
 public static class ManagedPointerTheory
 {
-    public static unsafe nint GetOffset<TGround, TTarget>
+    public static unsafe nint GetMemoryDisplacement<TGround, TTarget>
     (
-        ref readonly TGround refGround,
-        ref readonly TTarget refTarget
+        ref readonly TGround ground,
+        ref readonly TTarget target
     )
     {
-        nint groundAddress = (nint)Unsafe.AsPointer(ref Unsafe.AsRef(in refGround));
-        nint targetAddress = (nint)Unsafe.AsPointer(ref Unsafe.AsRef(in refTarget));
+        nint groundAddress = (nint)Unsafe.AsPointer(ref Unsafe.AsRef(in ground));
+        nint targetAddress = (nint)Unsafe.AsPointer(ref Unsafe.AsRef(in target));
         return targetAddress - groundAddress;
     }
 
-    public static unsafe ref TTarget GetFromOffset<TGround, TTarget>
+    public static unsafe ref TTarget GetFromMemoryDisplacement<TGround, TTarget>
     (
-        scoped ref readonly TGround refGround,
-        nint offset
+        scoped ref readonly TGround ground,
+        nint memoryDisplacement
     )
     {
-        nint groundAddress = (nint)Unsafe.AsPointer(ref Unsafe.AsRef(in refGround));
-        nint targetAddress = groundAddress + offset;
+        nint groundAddress = (nint)Unsafe.AsPointer(ref Unsafe.AsRef(in ground));
+        nint targetAddress = groundAddress + memoryDisplacement;
         return ref Unsafe.AsRef<TTarget>((void*)targetAddress);
+    }
+
+    public static void Method1
+    (
+        ReadOnlySpan<nint> layeredLengths,
+
+        out int index,
+        out nint length,
+        out nint offset
+    )
+    {
+
     }
 }
