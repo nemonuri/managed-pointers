d2a4920b4853972d14be33d0fa7417ffd9640a76
nemonuri <nemonuri2@gmail.com>
2025-03-07 22:33:06 +0900

    .

--- src/Nemonuri.ManagedPointers.Common/GlobalUsings.cs
+++ src/Nemonuri.ManagedPointers.Common/GlobalUsings.cs
@@ -1,3 +1,4 @@
 global using System.Runtime.CompilerServices;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Runtime.InteropServices;
+global using CommunityToolkit.Diagnostics;
--- src/Nemonuri.ManagedPointers.Common/ManagedPointerTheory.cs
+++ src/Nemonuri.ManagedPointers.Common/ManagedPointerTheory.cs
@@ -24,15 +24,93 @@ public static class ManagedPointerTheory
         return ref Unsafe.AsRef<TTarget>((void*)targetAddress);
     }
 
-    public static void Method1
+    public static bool TryGetSubSegment
     (
-        ReadOnlySpan<nint> layeredLengths,
+        ReadOnlySpan<nint> subSegmentsLengths,
+        nint firstSubSegmentBasedDisplacement,
+        out int foundSubSegmentIndex,
+        out nint foundSubSegmentLength,
+        out nint foundSubSegmentBasedDisplacement
+    )
+    {
+        Guard.IsGreaterThanOrEqualTo(firstSubSegmentBasedDisplacement, 0);
+        nint remainedDisplacement = firstSubSegmentBasedDisplacement;
 
-        out int index,
-        out nint length,
-        out nint offset
+        for (int i = 0; i < subSegmentsLengths.Length; i++)
+        {
+            nint currentSubSegmentLength = subSegmentsLengths[i];
+            Guard.IsGreaterThanOrEqualTo(currentSubSegmentLength, 0);
+            if (remainedDisplacement < currentSubSegmentLength)
+            {
+                foundSubSegmentIndex = i;
+                foundSubSegmentLength = currentSubSegmentLength;
+                foundSubSegmentBasedDisplacement = remainedDisplacement;
+                return true;
+            }
+            else
+            {
+                remainedDisplacement -= currentSubSegmentLength;
+            }
+        }
+
+        SetFailedResults(out foundSubSegmentIndex, out foundSubSegmentLength, out foundSubSegmentBasedDisplacement);
+        return false;
+
+        static void SetFailedResults(out int subSegmentIndex, out nint subSegmentLength, out nint subSegmentBasedDisplacement)
+        {
+            subSegmentIndex = default;
+            subSegmentLength = default;
+            subSegmentBasedDisplacement = default;
+        }
+    }
+
+    public static bool TryGetSegmentTreeLayout<T>
+    (
+        out int treeBreadth,
+        out int treeHeight
+    )
+    {
+    }
+
+    public static bool TryGetSegmentTree<T>
+    (
+        int treeBreadth,
+        int treeHeight,
+
+        Span<nint> subSegmentsLengthsFlattenedTreeDestination,
+        Span<nint> subSegmentsDegreesFlattenedTreeDestination,
+        Span<nint> leafSegmentsLengthsDestination
+    )
+    {
+    }
+
+    public static bool TryGetSubSegments<T>
+    (
+        int treeBreadth,
+        int treeHeight,
+        ReadOnlySpan<nint> subSegmentsLengthsFlattenedTree,
+        ReadOnlySpan<nint> subSegmentsDegreesFlattenedTree,
+        nint firstSubSegmentBasedDisplacement,
+
+        Span<int> foundSubSegmentIndexsDestination,
+        Span<nint> foundSubSegmentLengthsDestination,
+        Span<nint> foundSubSegmentBasedDisplacementsDestination
     )
     {
 
+    }
+
+    public static bool TryGetLogTextForSegmentTree<T>
+    (
+        int treeBreadth,
+        int treeHeight,
+        ReadOnlySpan<nint> subSegmentsLengthsFlattenedTree,
+        ReadOnlySpan<nint> subSegmentsDegreesFlattenedTree,
+        ReadOnlySpan<nint> leafSegmentsLengths,
+
+        out string resultLogText
+    )
+    {
+        
     }
 }
